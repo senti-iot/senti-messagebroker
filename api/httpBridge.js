7991fad2899f7023c0587fe88f50fd90977d0e99
Mikkel Broberg <mb@webhouse.dk>
2019-12-20 10:09:34 +0100

    bearer check on auth

--- api/httpBridge.js
+++ api/httpBridge.js
@@ -10,10 +10,32 @@ const logger = require('../logger/index').log
 
 const types = ['publish', 'state', 'config']
 
+const parseBearerToken = (req) => {
+	const auth = req.headers ? req.headers.authorization || null : null
+	if (!auth) {
+		return null
+	}
+
+	const parts = auth.split(' ')
+	// Malformed header.
+	if (parts.length < 2) {
+		return null
+	}
+
+	const schema = parts.shift().toLowerCase()
+	const token = parts.join(' ')
+	if (schema !== 'bearer') {
+		return null
+	}
+	return token
+}
 router.post('/:version/:customerID/location/:location/registries/:regID/:type', async (req, res, next) => {
 
 	let apiVersion = req.params.version
-	let authToken = req.headers.auth
+	let authToken = parseBearerToken(req)
+	if(authToken === null) {
+		authToken = req.headers.auth
+	}
 	let data = req.body
 
 	if (verifyAPIVersion(apiVersion)) {
@@ -43,7 +65,10 @@ router.post('/:version/:customerID/location/:location/registries/:regID/:type', 
 })
 router.post('/:version/:customerID/location/:location/registries/:regID/devices/:deviceName/:type', async (req, res) => {
 	let apiVersion = req.params.version
-	let authToken = req.headers.auth
+	let authToken = parseBearerToken(req)
+	if(authToken === null) {
+		authToken = req.headers.auth
+	}
 	let data = req.body
 	// req.log.info("Received data from:", req.url)
 	// log.info("Received data from:", req.url)
