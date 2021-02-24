4f4f2b47169ff4ba905d041ca3109ff074e1087f
Mikkel Broberg <mb@webhouse.dk>
2021-02-24 10:12:35 +0100

    added ttn v3 stack

--- api/ttn.js
+++ api/ttn.js
@@ -59,4 +59,40 @@ router.post('/v1/ttn-application', async (req, res, next) => {
 		res.send(`API/httpBridge version: ${apiVersion} not supported`)
 	}
 })
+
+router.post('/v1/ttn-application-v3', async (req, res, next) => {
+	let apiVersion = 'v1'
+	let authToken = parseBearerToken(req)
+	if (authToken === null) {
+		authToken = (req.headers.auth) ? req.headers.auth : req.query.auth
+	}
+
+	let data = req.body
+	if (typeof data === 'string') {
+		try {
+			data = JSON.parse(data)
+		}
+		catch (e) {
+			res.send('Invalid Data package').status(400)
+		}
+	}
+	if (verifyAPIVersion(apiVersion)) {
+		if (authenticate(authToken)) {
+			// res.json('API/httpBridge POST Access Authenticated!')
+			console.log('API/httpBridge POST Access Authenticated!')
+			//Send the data to DataBroker
+			secureMqttClient.sendMessage(req.path.substr(1, req.path.length), JSON.stringify(data))
+			res.status(200).json()
+		} else {
+			res.status(403).json('Unauthorized Access! 403')
+			console.log('TTN Unauthorized Access!')
+			console.log(req.params)
+			console.log(req.headers)
+			console.log(req.body)
+		}
+	} else {
+		console.log(`API/httpBridge version: ${apiVersion} not supported`)
+		res.send(`API/httpBridge version: ${apiVersion} not supported`)
+	}
+})
 module.exports = router
