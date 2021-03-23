f98307d36c90179b70831fce738d868cb3634e06
Andrei Tudor <andrei.tudor27@gmail.com>
2021-03-23 14:31:15 +0100

    update + log everything

---
+++ api/routeLogger.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.all('*', (req, res, next) => {
+	console.log('Path:', req.path)
+	console.log('Body:', req.body)
+});
--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const helmet = require('helmet')
 const app = express()
 // const pino = require('pino')
 // const fs = require('fs')
-const logger = require('./logger/index').log
-const bodyParser = require('body-parser')
+// const logger = require('./logger/index').log
 // module.exports.log=pino(pino.destination(`/var/log/nodejs/messagebroker/${new Date().toLocaleDateString().replace(/\//g, '-')}-others.json`))
 // const logger=pino(pino.destination(`/var/log/nodejs/databroker/${new Date().toLocaleDateString().replace(/\//g, '-')}.json`))
-module.exports.log = logger
+// module.exports.log = logger
 // const mqttHandler = require('./mqtt/receiveData')
 // const expressPino = require('express-pino-logger')({
 // 	logger: logger
@@ -27,13 +26,14 @@ module.exports.secureMqttClient = secureMqttClient
 const ttnApi = require('./api/ttn')
 const comadanApi = require('./api/comadan')
 const httpBridge = require('./api/httpBridge')
+const routeLogger = require('./api/routeLogger')
 
 const port = process.env.NODE_PORT || 3003
 
 app.use(helmet())
-app.use(bodyParser.json())
-app.use(bodyParser.text())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
@@ -44,13 +44,14 @@ app.use(cors())
 // app.use('/apiversion', apiVersionRouter)
 // app.use('/template', templateRouter)
 app.use('/', ttnApi, comadanApi, httpBridge)
+app.use([routeLogger])
 //---Start the express server---------------------------------------------------
 
 
 const startAPIServer = () => {
 	app.listen(port, () => {
 		console.log('Senti Message Broker started on port:', port)
-		logger(`Senti Message Broker listening on port ${port}`, 'info')
+		// logger(`Senti Message Broker listening on port ${port}`, 'info')
 	}).on('error', (err) => {
 		if (err.errno === 'EADDRINUSE') {
 			console.log('Server not started, port ' + port + ' is busy')
