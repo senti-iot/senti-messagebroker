3bde6eb2f56fcbeeee96e68662488c8de1d1ae1c
Mikkel Broberg <mb@webhouse.dk>
2021-04-27 15:33:43 +0200

    comadan return

--- api/comadan.js
+++ api/comadan.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const verifyAPIVersion = require('senti-apicore').verifyapiversion
 const { authenticate } = require('senti-apicore')
 const secureMqttClient = require('../server').secureMqttClient
+var mysqlConn = require('../mysql/mysql_handler')
 
 
 router.post('/v1/comadan-application', async (req, res, next) => {
@@ -24,8 +25,19 @@ router.post('/v1/comadan-application', async (req, res, next) => {
 			//Send the data to DataBroker
 			console.log('/v1/comadan-application ok')
 			console.log(data)
-			secureMqttClient.sendMessage(req.path.substr(1, req.path.length), JSON.stringify(data))
-			res.status(200).json()
+			// secureMqttClient.sendMessage(req.path.substr(1, req.path.length), JSON.stringify(data))
+			let result = ""
+			if (data.recv === true) {
+				let select = `SELECT d.metadata
+								FROM device d
+								WHERE d.uuname = ? AND d.deleted = 0;`
+				let rs = await mysqlConn.query(select, [data.ID])
+				console.log(rs[0])
+				if (rs[0].length === 1) {
+					result = JSON.parse(rs[0][0].metadata.comaConfig)
+				}
+			}
+			res.status(200).json(result)
 		} else {
 			res.status(403).json('Unauthorized Access! 403')
 			console.log('Comadan Unauthorized Access!')
