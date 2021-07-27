e89577a60d89b3ddeab0bbca08eb2396498af8e7
Mikkel Broberg <mb@webhouse.dk>
2021-07-27 11:42:42 +0200

    more test

--- api/httpBridge.js
+++ api/httpBridge.js
@@ -58,6 +58,7 @@ router.post('/:version/:customerID/location/:location/registries/:regID/:type', 
 
 			//Send the data to DataBroker
 			console.log(req.url.substr(1, req.url.length),JSON.stringify({...data, ...req.params }))
+			console.log(JSON.stringify(data))
 			//dataBrokerChannel.sendMessage(req.url.substr(1, req.url.length), JSON.stringify(data))
 			secureMqttClient.sendMessage(req.path.substr(1, req.path.length), JSON.stringify(data))
 			res.status(200).json()
